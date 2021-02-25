74208dec4c02fef2988253ef4f43a324c63f50ab
Jack Hancock <77704393+JackalGame@users.noreply.github.com>
2021-02-25 19:21:24 +0000

    Lights Script Added

---
+++ Lights.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Lights : MonoBehaviour
+{
+    [SerializeField] int activationDistance = 60;
+
+    Transform player;
+    float playerDistance;
+    Light[] lights;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerHealth>().transform;
+        lights = GetComponentsInChildren<Light>();
+    }
+
+    void Update()
+    {
+        playerDistance = Vector3.Distance(transform.position, player.position);
+
+        if(playerDistance < activationDistance)
+        {
+            foreach(Light light in lights)
+            {
+                light.enabled = true;
+            }
+        }
+        else
+        {
+            foreach(Light light in lights)
+            {
+                light.enabled = false;
+            }
+        }
+    }
+}
